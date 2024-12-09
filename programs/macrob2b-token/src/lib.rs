269917915c8881024dcc6288d65b9e11a1e75acf
Sadeq Naderi <sadeq.naderi@gmail.com>
2024-12-10 00:22:42 +0330

    Enable submit proposal

--- programs/macrob2b-token/src/lib.rs
+++ programs/macrob2b-token/src/lib.rs
@@ -1,15 +1,37 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Hs5A3fAN5GpEVpHPSZtmAix8RPf171Ry89EQQkVtoVwP");
+declare_id!("BFQBgQicbMZUvgbC6AkLQoevG8QYR7ryf42EpDTCMcko");
 
 #[program]
-pub mod macrob2b_token {
+pub mod macrob2b_program {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn submit_proposal(
+        ctx: Context<SubmitProposal>,
+        title: String,
+        brief: String,
+    ) -> Result<()> {
+        let proposal = &mut ctx.accounts.proposal;
+        proposal.owner = ctx.accounts.user.key();
+        proposal.title = title;
+        proposal.brief = brief;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SubmitProposal<'info> {
+    #[account(init,payer=user,space=8+32+104+804)]
+    pub proposal: Account<'info, Proposal>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Proposal {
+    pub owner: Pubkey,
+    pub title: String,
+    pub brief: String,
+}
--- tests/macrob2b-token.ts
+++ tests/macrob2b-token.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Macrob2bToken } from "../target/types/macrob2b_token";
+import { Macrob2bProgram } from "../target/types/macrob2b_program";
 
-describe("macrob2b-token", () => {
+describe("macrob2b-program", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Macrob2bToken as Program<Macrob2bToken>;
+  const program = anchor.workspace.Macrob2bToken as Program<Macrob2bProgram>;
 
   it("Is initialized!", async () => {
     // Add your test here.
